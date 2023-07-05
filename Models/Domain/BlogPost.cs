8d71f9da9b6b5db5da361e5ed8687afb5caba9f4
Anish Joshi <anish.joshi@cedargate.com>
2023-07-05 09:34:24 +0545

    added domain model

---
+++ Models/Domain/BlogPost.cs
@@ -0,0 +1,20 @@
+﻿namespace blog.Models.Domain
+{
+    // Represents a blog post
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        // Blog post can have multiple tags
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ Models/Domain/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace blog.Models.Domain
+{
+    // Represents a tag
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? DisplayName { get; set; }
+
+        // Tags can have multiple blog posts
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+
+}
