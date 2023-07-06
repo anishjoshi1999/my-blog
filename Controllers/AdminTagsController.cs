12bce6d45807f7b58d920255eb9f1cab139a0506
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-07-06 07:54:43 +0545

    Tags Controller: Admin Functionality to add a Tag

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blog.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        //https://localhost:7193/AdminTags/Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
