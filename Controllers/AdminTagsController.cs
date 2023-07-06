26d258b4dfd1e43c7f6c1f4e24f769d1a7527a86
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-07-06 08:25:16 +0545

    saving tags data into database

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,10 +1,18 @@
-﻿using blog.Models.ViewModels;
+﻿using blog.Data;
+using blog.Models.Domain;
+using blog.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogDbContext blogDbContext;
+        //Constructor injection
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+           this.blogDbContext = blogDbContext;
+        }
         //https://localhost:7193/AdminTags/Add
         [HttpGet]
         public IActionResult Add()
@@ -17,8 +25,16 @@ namespace blog.Controllers
         {
             //var name = Request.Form["name"];
             //var displayName = Request.Form["displayName"];
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
+
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+            };
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges();
             return View("Add");
         }
     }
